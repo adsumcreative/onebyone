e07dc016d3f912a11bf366bdb93d532f36040283
junereycasuga <junerey.casuga@gmail.com>
2013-09-23 21:28:52 +0800

    update on index.php

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/../framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
+if(file_exists(dirname(__FILE__.'/protected/config/main_dev.php'))){
+	$config=dirname(__FILE__).'/protected/config/main_dev.php';
+} else {
+	$config=dirname(__FILE__).'/protected/config/main.php';
+}
 
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
